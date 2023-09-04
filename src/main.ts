51062456a2931fbe5caed75967113544e61b7f0f
Lucas Vogel <contact@vogelino.com>
2023-09-04 15:00:45 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import "./style.css";
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg") as HTMLCanvasElement,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+scene.background = new THREE.Color(0xffffff);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+animate();
